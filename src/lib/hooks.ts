70006ca78e9d28ac94ebc5df752e670658610d69
Pablo S <cineschonffeldt@gmail.com>
2025-06-23 14:46:12 -0500

    Fix loading animation on onitial load

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -15,7 +15,7 @@ const fetchJobItem = async (id: number): Promise<JobItemApiResponse> => {
 };
 
 export function useJobItem(id: number | null) {
-  const { data, isLoading } = useQuery(
+  const { data, isInitialLoading } = useQuery(
     ["job-item", id],
     () => (id ? fetchJobItem(id) : null),
     {
@@ -27,6 +27,7 @@ export function useJobItem(id: number | null) {
     }
   );
   const jobItem = data?.jobItem;
+  const isLoading = isInitialLoading;
   return { jobItem, isLoading } as const;
 }
 
