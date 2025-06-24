1955314332787e835e801aa1be0b0544a2b7a813
Pablo S <cineschonffeldt@gmail.com>
2025-06-24 13:43:31 -0500

    Add constant for max results per page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -14,6 +14,7 @@ import ResultsCount from "./ResultsCount";
 import SortingControls from "./SortingControls";
 import { useDebounce, useJobItems } from "../lib/hooks";
 import { Toaster } from "react-hot-toast";
+import { RESULTS_PER_PAGE } from "../lib/constants";
 
 function App() {
   // state
@@ -25,9 +26,12 @@ function App() {
 
   // derived / computedstate
   const totalNumberOfResults = jobItems?.length || 0;
-  const totalNumberOfPages = totalNumberOfResults / 7;
+  const totalNumberOfPages = totalNumberOfResults / RESULTS_PER_PAGE;
   const jobItemsSliced =
-    jobItems?.slice(currentPage * 7 - 7, currentPage * 7) || [];
+    jobItems?.slice(
+      currentPage * RESULTS_PER_PAGE - RESULTS_PER_PAGE,
+      currentPage * RESULTS_PER_PAGE
+    ) || [];
 
   // event handlers / actions
   const handleChangePage = (direction: "next" | "previous") => {
