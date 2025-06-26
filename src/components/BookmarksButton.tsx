fad6589b450a899ce0840c35a7bd6dbd3b74a41c
Pablo S <cineschonffeldt@gmail.com>
2025-06-26 16:26:58 -0500

    Turn close popover to custom hook

--- src/components/BookmarksButton.tsx
+++ src/components/BookmarksButton.tsx
@@ -1,35 +1,25 @@
 import { TriangleDownIcon } from "@radix-ui/react-icons";
 import BookmarksPopover from "./BookmarksPopover";
-import { useEffect, useState } from "react";
+import { useRef, useState } from "react";
+import { useOnClickOutside } from "../lib/hooks";
 
 export default function BookmarksButton() {
   const [isOpen, setIsOpen] = useState(false);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const popoverRef = useRef<HTMLDivElement>(null);
+  useOnClickOutside([buttonRef, popoverRef], () => setIsOpen(false));
 
-  useEffect(() => {
-    const handleClick = (e: MouseEvent) => {
-      if (
-        e.target instanceof HTMLElement &&
-        !e.target.closest(".bookmarks-btn") &&
-        !e.target.closest(".bookmarks-popover")
-      ) {
-        setIsOpen(false);
-      }
-    };
-    document.addEventListener("click", handleClick);
-
-    return () => {
-      document.removeEventListener("click", handleClick);
-    };
-  }, []);
   return (
     <section>
       <button
+        ref={buttonRef}
         onClick={() => setIsOpen((prev) => !prev)}
         className="bookmarks-btn"
       >
         Bookmarks <TriangleDownIcon />
       </button>
-      {isOpen && <BookmarksPopover />}
+
+      {isOpen && <BookmarksPopover ref={popoverRef} />}
     </section>
   );
 }
--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -145,6 +145,27 @@ export function useLocalStorage<T>(
   return [value, setValue] as const;
 }
 
+export function useOnClickOutside(
+  refs: React.RefObject<HTMLElement>[],
+  handler: () => void
+) {
+  useEffect(() => {
+    const handleClick = (e: MouseEvent) => {
+      if (refs.every((ref) => !ref.current?.contains(e.target as Node))) {
+        handler();
+      }
+    };
+
+    document.addEventListener("click", handleClick);
+
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  }, [refs, handler]);
+}
+
+// --------------------
+
 export function useBookmarksContext() {
   const context = useContext(BookmarksContext);
   if (!context) {
